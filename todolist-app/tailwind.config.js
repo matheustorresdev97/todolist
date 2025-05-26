e50587209ec05aabdb874dab5e92f2cfe73c255a
matheustorresdev97 <matheustorresdev@gmail.com>
2025-05-26 09:39:43 -0300

    feat: add colors theme

--- todolist-app/tailwind.config.js
+++ todolist-app/tailwind.config.js
@@ -1,9 +1,12 @@
+import { colors } from "./src/styles/colors.ts"
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors
+    },
   },
   plugins: [],
 }
